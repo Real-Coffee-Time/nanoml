63dcb4dd896c4b9946214749b368181df6f0a296
Anatole-DC <adechauveron@gmail.com>
2021-05-06 19:52:44 +0200

    nanoml loader H

--- nanoml_loader/p_nanoml_loader.h
+++ nanoml_loader/p_nanoml_loader.h
@@ -55,9 +55,56 @@ typedef t_nanoml* a_nanoml;
 
 /* ========================= WORDS ========================= */
 
+/***/
+a_word create_word(string word, int is_important);
+
+/***/
+a_text create_text();
+
+/***/
+int add_word_to_text(a_text text, a_word word);
+
+/***/
+int print_word(a_word word);
+
+/***/
+int print_text(a_text text);
+
 /* ========================= LISTS ========================= */
+
+/***/
+a_list init_empty_list();
+
+/***/
+int update_list(a_text text);
+
+/***/
+int add_sublist(a_text list);
 
 /* ======================== CONTENT ======================== */
 
+/***/
+a_content init_empty_content();
+
+/***/
+int add_text_to_content(a_text text, a_content content);
+
+/***/
+int add_list_to_content(a_list list, a_content content);
+
+/***/
+int add_subcontent_to_content(a_content subcontent, a_content content);
+
+/***/
+int print_content(a_content content)
+
+/***/
+a_section init_empty_section();
+
+/***/
+int add_content_to_section(a_content content, a_section section);
+
+/***/
+int print_section(a_section section);
 
 #endif
